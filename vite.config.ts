25b8d5e895e75c9fc061252610e724cdc8b492c2
Ashton Doane <ashtondoane@Ashtons-MacBook-Air.local>
2024-07-25 01:35:56 -0700

    Reverted vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/pallanckLabTools/",
   plugins: [react()],
-  root: '',
-  build: {
-    outDir: 'build',
-    emptyOutDir: true, // also necessary
-  }
 })
