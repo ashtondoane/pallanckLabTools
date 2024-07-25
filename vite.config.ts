3a8a8cb3307b3ba1f744ecd095f181501c66e230
Ashton Doane <ashtondoane@Ashtons-MacBook-Air.local>
2024-07-25 01:25:30 -0700

    Test Fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/pallanckLabTools/",
   plugins: [react()],
+  root: '',
+  build: {
+    outDir: 'build',
+    emptyOutDir: true, // also necessary
+  }
 })
